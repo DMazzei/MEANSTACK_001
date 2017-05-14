7e5f691c83d933bfb9dc88706c5b3bb246b51111
Daniel Mazzei <danielmazzei@gmail.com>
2017-05-14 23:00:34 +0100

    done.

--- MEANStack_001/public/js/app.js
+++ MEANStack_001/public/js/app.js
@@ -1,2 +1,2 @@
 // public/js/app.js
-angular.module('starter-node-angular', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService']);
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService']);
